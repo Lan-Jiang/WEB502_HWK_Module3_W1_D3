49af02a29d80619b3835887c4ce0e45c2e223c50
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-03-11 22:30:58 -0500

    WEB502 HWK M3 W1 D3 updated

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 var express = require('express');
 var path = require('path');
-var routes = require('./routes/index')
 var app = express();
 
 const port = process.env.PORT || 3000;
-// app.use('/', routes);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, './routes/food_blog.html'));
-  res.render('blog');
+app.get('/', function(req,res) {
+  res.render('content');
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
-
-
-
-
-
-
-
 
